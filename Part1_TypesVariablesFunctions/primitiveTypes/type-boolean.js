398e38ca6ac51995fed82f65f6cf4674deb18949
Joao Pedro Barbosa Leite <jbarbos@lifescan.com>
2023-06-12 13:51:28 -0300

    Renaming folder

---
+++ Part1_TypesVariablesFunctions/primitiveTypes/type-boolean.js
@@ -0,0 +1,16 @@
+// Boolean type
+
+const num1 = 1;
+const num2 = 2;
+const num3 = 2;
+
+console.log(num1 === num2);
+console.log(num2 === num3);
+
+const text1 = "text";
+const text2 = "TeXt";
+
+console.log(text1 === text2);
+console.log(text1 === text2.toLowerCase());
+
+let somethingIsLocked = false;
